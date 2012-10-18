93f8f0daeaea69b7c45e2b883c1084b3c7088641
takewo <st0098@gmail.com>
2012-10-18 19:25:02 +0900

    answer of Q:0005

---
+++ Volume0/0005_GCD and LCM/Main.rb
@@ -0,0 +1,26 @@
+#coding: UTF-8
+class EuclideanAlgorithmCalcurator
+  def get_gcd(m, n)
+    if n == 0
+      return m;
+    end
+    return get_gcd(n, m % n)
+  end
+  def get_lcd(m, n)
+    return (m * n) / get_gcd(m, n)
+  end
+end
+
+if __FILE__ == $0
+  while line = gets
+    args = line.split(" ").map{|e|e.to_i}
+    m = args[0] >= args[1] ? args[0] : args[1]
+    n = m == args[0] ? args[1] : args[0]
+ 
+    calcurator = EuclideanAlgorithmCalcurator.new
+    # 最大公約数
+    puts "#{calcurator.get_gcd(m, n)} #{calcurator.get_lcd(m, n)}"
+  end
+end
+
+
